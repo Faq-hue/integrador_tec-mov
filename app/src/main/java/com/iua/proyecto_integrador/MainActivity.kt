54da12b11ea6978124d3a39fc040700a43c56290
Faq <fzamora994@alumnos.iua.edu.ar>
2022-10-24 11:45:14 -0300

    Añadido primer fragment
    Falta implementacion

--- app/src/main/java/com/iua/proyecto_integrador/MainActivity.kt
+++ app/src/main/java/com/iua/proyecto_integrador/MainActivity.kt
@@ -2,10 +2,11 @@ package com.iua.proyecto_integrador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_incial)
+        setContentView(R.layout.activity_login)
     }
 }
